ae699e6d942ea7277789ffb203f4340876dbbe51
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-09-02 16:33:54 -0700

    basic fill rect test and addition of site styling

--- src/app.js
+++ src/app.js
@@ -7,8 +7,14 @@ class App {
       console.error(msg);
       throw new Error(msg);
     }
-    const ctx = canvas.getContext('2d', { alpha: false });
+
     console.log('started letter-sketch');
+
+    const ctx = canvas.getContext('2d', { alpha: false });
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+    ctx.clearRect(40, 40, canvas.width - 80, canvas.height - 80);
   }
 }
 
