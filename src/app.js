a506cafcf9c184461e46a7546d77f85accc3194b
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-09-04 22:37:27 -0700

    scale hidpi canvas to de-blurrify text

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 export default {
-  start: doc => {
+  start: (win, doc) => {
     const canvas = doc.getElementById('sketchpad');
     if (!canvas.getContext) {
       const msg = 'No console support detected!';
@@ -9,11 +9,23 @@ export default {
 
     console.log('started letter-sketch');
 
+    // TODO: this includes the border so calculations off rect are slightly off
+    const rect = canvas.getBoundingClientRect();
+    const dpr = win.devicePixelRatio || 1;
+    console.log('dpr: %d, rect: %d x, %d y', dpr, rect.width, rect.height);
+
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+
     const ctx = canvas.getContext('2d', { alpha: false });
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.scale(dpr, dpr);
+
+    ctx.clearRect(0, 0, rect.width, rect.height);
     ctx.fillStyle = 'yellow';
-    ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
-    ctx.clearRect(40, 40, canvas.width - 80, canvas.height - 80);
+    console.log('fill rect %d w %d h', rect.width - 20, rect.height - 20);
+    ctx.fillRect(10, 10, rect.width - 20, rect.height - 20);
+    ctx.clearRect(40, 40, rect.width - 80, rect.height - 80);
+    
     ctx.font = '12px Monaco';
     ctx.fillStyle = 'red';
     ctx.fillText('Hello World!', 25, 25);
