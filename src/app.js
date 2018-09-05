474638da4cfc313e43ddf18faa6d77e84d449234
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-09-04 22:15:27 -0700

    remove premature abstraction and add text output

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-class App {
-  constructor(doc) {
-    this.doc = doc;
+export default {
+  start: doc => {
     const canvas = doc.getElementById('sketchpad');
     if (!canvas.getContext) {
       const msg = 'No console support detected!';
@@ -15,13 +14,8 @@ class App {
     ctx.fillStyle = 'yellow';
     ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
     ctx.clearRect(40, 40, canvas.width - 80, canvas.height - 80);
-  }
-}
-
-let app;
-
-export default {
-  start: function (doc) {
-    app = new App(doc);
+    ctx.font = '12px Monaco';
+    ctx.fillStyle = 'red';
+    ctx.fillText('Hello World!', 25, 25);
   }
 };
