2ec01695897e61944475a8b66143ee37d709956b
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-11-17 22:38:38 -0800

    implement basic notifier

--- src/refresh.js
+++ src/refresh.js
@@ -1,9 +1,28 @@
 export class ViewNotifier {
-  subscribe() {
-
+  constructor() {
+    // TODO: defaultdict-like
+    this._notifications = {};
   }
 
-  notify(event) {
+  register(views) {
+    for (const view of views) {
+      view.subscribe(this);
+    }
+  }
 
+  subscribe(event, handler) {
+    if (!this._notifications[event]) {
+      this._notifications[event] = []
+    }
+    this._notifications[event].push(handler);
+  }
+
+  signal(update) {
+    const notification = this._notifications[update.event];
+    if (!notification) return;
+
+    for (const handler of notification) {
+      handler(update);
+    }
   }
 }
