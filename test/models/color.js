b5465f98cde0a96856cbf50213c47ef5b91e7ca5
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-11-22 21:58:38 -0800

    rework test format slightly

--- test/models/color.js
+++ test/models/color.js
@@ -2,9 +2,7 @@ import {expect} from 'chai';
 import {Color, cssHexToColor, colorToCssHex} from '../../src/models/color.js';
 
 describe('Color', function () {
-
   describe('#staticProperties', function () {
-
     it('exposes white constant', function () {
       const c = Color.WHITE;
       
@@ -22,14 +20,13 @@ describe('Color', function () {
       expect(c.b).to.equal(0x00);
       expect(c.a).to.equal(0xff);
     });
-
   });
-
+  
   describe('#ctor()', function () {
-
     it('sets rgb', function () {
-      const r = 0x44, g = 0x30, b = 0xab,
-            c = new Color(r, g, b);
+      const r = 0x44, g = 0x30, b = 0xab;
+      
+      const c = new Color(r, g, b);
 
       expect(c.r).to.equal(r);
       expect(c.g).to.equal(g);
@@ -39,8 +36,9 @@ describe('Color', function () {
 
     it('sets rgba', function () {
       const r = 0x44, g = 0x30,
-            b = 0xab, a = 0x78,
-            c = new Color(r, g, b, a);
+            b = 0xab, a = 0x78;
+
+      const c = new Color(r, g, b, a);
 
       expect(c.r).to.equal(r);
       expect(c.g).to.equal(g);
@@ -50,24 +48,23 @@ describe('Color', function () {
 
     it('clamps invalid values', function () {
       const r = -20, g = 256,
-            b = -1, a = 1024,
-            c = new Color(r, g, b, a);
+            b = -1, a = 1024;
+      
+      const c = new Color(r, g, b, a);
 
       expect(c.r).to.equal(0x00);
       expect(c.g).to.equal(0xff);
       expect(c.b).to.equal(0x00);
       expect(c.a).to.equal(0xff);
     });
-
   });
-
 });
 
 describe('#cssHexToColor()', function () {
-
   it('parses rgb css hex string', function () {
-    const s = '#23ab45',
-          c = cssHexToColor(s);
+    const s = '#23ab45';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x23);
     expect(c.g).to.equal(0xab);
@@ -76,8 +73,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses rgb missing leading hash', function () {
-    const s = '23ab45',
-          c = cssHexToColor(s);
+    const s = '23ab45';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x23);
     expect(c.g).to.equal(0xab);
@@ -86,8 +84,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses rgba css hex string', function () {
-    const s = '#23ab45bc',
-          c = cssHexToColor(s);
+    const s = '#23ab45bc';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x23);
     expect(c.g).to.equal(0xab);
@@ -96,8 +95,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses rgba missing leading hash', function () {
-    const s = '23ab45bc',
-          c = cssHexToColor(s);
+    const s = '23ab45bc';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x23);
     expect(c.g).to.equal(0xab);
@@ -106,8 +106,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses short rgb css hex string', function () {
-    const s = '#2a4',
-          c = cssHexToColor(s);
+    const s = '#2a4';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x22);
     expect(c.g).to.equal(0xaa);
@@ -116,8 +117,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses short rgb missing leading hash', function () {
-    const s = '2a4',
-          c = cssHexToColor(s);
+    const s = '2a4';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x22);
     expect(c.g).to.equal(0xaa);
@@ -126,8 +128,9 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses short rgba css hex string', function () {
-    const s = '#2a4b',
-          c = cssHexToColor(s);
+    const s = '#2a4b';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x22);
     expect(c.g).to.equal(0xaa);
@@ -136,17 +139,16 @@ describe('#cssHexToColor()', function () {
   });
 
   it('parses short rgba missing leading hash', function () {
-    const s = '2a4b',
-          c = cssHexToColor(s);
+    const s = '2a4b';
+    
+    const c = cssHexToColor(s);
 
     expect(c.r).to.equal(0x22);
     expect(c.g).to.equal(0xaa);
     expect(c.b).to.equal(0x44);
     expect(c.a).to.equal(0xbb);
   });
-
 });
 
 describe('#colorToCssHex()', function () {
-
 });
