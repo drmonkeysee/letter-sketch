f96b851579ea0f06f1d2af01d53025859de5b356
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-11-24 23:06:30 -0800

    delegate color selection to helper views

--- src/views/palette.js
+++ src/views/palette.js
@@ -3,21 +3,84 @@ import {EVENTS} from '../refresh.js';
 import {COMMANDS} from '../commands.js';
 import {channelsToCss, COLORS} from '../models/color.js';
 
+class ColorSelection extends View {
+  constructor(id, cmd, refreshEvent, onSelected, initColor, ...args) {
+    super(...args);
+    this.selection = this._doc.getElementById(id);
+    this.cmd = cmd;
+    this.refreshEvent = refreshEvent;
+    this.onSelected = onSelected;
+    this.initColor = initColor;
+  }
+
+  draw() {
+    this._setColor(this.initColor);
+    this.selection.addEventListener('click', e => this.onSelected(this));
+  }
+
+  subscribe(notifier) {
+    notifier.subscribe(this.refreshEvent, this._refreshColor.bind(this));
+  }
+
+  select() {
+    this.selection.classList.add('selected');
+  }
+
+  deselect() {
+    this.selection.classList.remove('selected');
+  }
+
+  pick(color) {
+    this._dispatch.command(this.cmd, color);
+  }
+
+  _refreshColor(update) {
+    this._setColor(update.color);
+  }
+
+  _setColor(color) {
+    if (color) {
+      this.selection.classList.remove('no-color');
+      this.selection.style.backgroundColor = color;
+    } else {
+      this.selection.classList.add('no-color');
+      this.selection.style.backgroundColor = null;
+    }
+  }
+}
+
 export class ColorPalette extends View {
   constructor(...args) {
     super(...args);
     this._palette = this._doc.getElementById('palette');
+    const onSelectedHandler = this._setSelection.bind(this);
     this._colorSelections = [
-      this._doc.getElementById('foreground-selection'),
-      this._doc.getElementById('background-selection'),
-      this._doc.getElementById('fill-selection')
+      new ColorSelection(
+        'foreground-selection',
+        COMMANDS.setForegroundColor,
+        EVENTS.onForegroundColorChanged,
+        onSelectedHandler,
+        COLORS.black, // TODO: figure out how to get initial color from brush
+        ...args
+      ),
+      new ColorSelection(
+        'background-selection',
+        COMMANDS.setBackgroundColor,
+        EVENTS.onBackgroundColorChanged,
+        onSelectedHandler,
+        null,
+        ...args
+      ),
+      new ColorSelection(
+        'fill-selection',
+        COMMANDS.setFillColor,
+        EVENTS.onFillColorChanged,
+        onSelectedHandler,
+        null,
+        ...args
+      )
     ];
-    [this._fgSelection, this._bgSelection, this._fillSelection] = this._colorSelections;
     this._currentSelection = null;
-  }
-
-  subscribe(notifier) {
-    notifier.subscribe(EVENTS.onForegroundColorChanged, this._refreshColor.bind(this));
   }
 
   draw() {
@@ -37,45 +100,27 @@ export class ColorPalette extends View {
       }
     }
 
-    // TODO: figure out how to get this from brush
-    const fgColor = COLORS.black,
-          bgColor = null,
-          fillColor = null;
-    this._setColorSelection(this._fgSelection, fgColor);
-    this._setColorSelection(this._bgSelection, bgColor);
-    this._setColorSelection(this._fillSelection, fillColor);
-
-    const handler = e => this._setCurrentSelection(e.target);
-    for (const selection of this._colorSelections) {
-      selection.addEventListener('click', handler);
+    for (const selectionView of this._colorSelections) {
+      selectionView.draw();
     }
-    this._setCurrentSelection(this._fgSelection);
+    this._setSelection(this._colorSelections[0]);
   }
 
-  _setCurrentSelection(targetSelection) {
-    for (const selection of this._colorSelections) {
-      selection.classList.remove('selected');
+  subscribe(notifier) {
+    for (const selectionView of this._colorSelections) {
+      selectionView.subscribe(notifier);
     }
-    targetSelection.classList.add('selected');
-    this._currentSelection = targetSelection;
+  }
+
+  _setSelection(selectionView) {
+    for (const selectionView of this._colorSelections) {
+      selectionView.deselect();
+    }
+    selectionView.select();
+    this._currentSelection = selectionView;
   }
 
   _pickColor(event) {
-    // TODO: reverse dispatch to ColorSelection view to determine fg, bg, fill
-    this._dispatch.command(COMMANDS.setForegroundColor, event.target.style.backgroundColor);
-  }
-
-  _refreshColor(update) {
-    this._fgSelection.style.backgroundColor = update.color;
-  }
-
-  _setColorSelection(selection, color) {
-    if (color) {
-      selection.classList.remove('no-color');
-      selection.style.backgroundColor = color;
-    } else {
-      selection.classList.add('no-color');
-      selection.style.backgroundColor = null;
-    }
+    this._currentSelection.pick(event.target.style.backgroundColor);
   }
 }
