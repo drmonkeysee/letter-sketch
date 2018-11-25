371963929f23fb1e0e83ec73367ea809953153e3
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-11-24 17:40:01 -0800

    finish up namemap tests

--- test/models/namemap.js
+++ test/models/namemap.js
@@ -9,28 +9,69 @@ describe('#makeNamemap', function () {
       }
       return Array.from(gen(count));
     };
-
-    this.mapItemVal = (n, i) => `${name}-${val}`;
+    this.mapItem = (n, i) => `${i.name.slice(0, -1)}(${n}-${10 + i.val})`;
   });
 
   it('creates empty map if empty items', function () {
     const items = this.makeItems();
 
-    const result = makeNamemap(items, this.mapItemVal);
+    const result = makeNamemap(items, this.mapItem);
 
-    const expected = {};
-    expect(result).to.equal(expected);
+    expect(result).to.be.empty;
   });
 
   it('creates map from named items', function () {
-    expect.fail('not implemented');
+    const items = this.makeItems(3);
+
+    const result = makeNamemap(items, this.mapItem);
+
+    const expected = {
+      foo0: 'Foo(foo0-10)',
+      foo1: 'Foo(foo1-11)',
+      foo2: 'Foo(foo2-12)'
+    };
+    expect(result).to.be.deep.equal(expected);
   });
 
   it('uses custom name mapper', function () {
-    expect.fail('not implemented');
+    const items = this.makeItems(3),
+          mapName = name => name.toUpperCase();
+
+    const result = makeNamemap(items, this.mapItem, mapName);
+
+    const expected = {
+      FOO0: 'Foo(foo0-10)',
+      FOO1: 'Foo(foo1-11)',
+      FOO2: 'Foo(foo2-12)'
+    };
+    expect(result).to.be.deep.equal(expected);
   });
 
   it('creates map from string items', function () {
-    expect.fail('not implemented');
+    const items = ['Foo', 'Bar', 'Baz'],
+          mapItem = (n, i) => `${n}(${i})`;
+    
+    const result = makeNamemap(items, mapItem);
+
+    const expected = {
+      foo: 'foo(Foo)',
+      bar: 'bar(Bar)',
+      baz: 'baz(Baz)'
+    };
+    expect(result).to.be.deep.equal(expected);
+  });
+
+  it('creates map from string objects', function () {
+    const items = [new String('Foo'), new String('Bar'), new String('Baz')],
+          mapItem = (n, i) => `${n}(${i})`;
+    
+    const result = makeNamemap(items, mapItem);
+
+    const expected = {
+      foo: 'foo(Foo)',
+      bar: 'bar(Bar)',
+      baz: 'baz(Baz)'
+    };
+    expect(result).to.be.deep.equal(expected);
   });
 });
