883849418e94fcf4ac33da2ff7c446bdeb85724b
Brandon Stansbury <brandonrstansbury@gmail.com>
2018-12-01 17:15:45 -0800

    move canvas into sketchpad view

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,11 @@
 import {GlyphRuler} from './ruler.js';
 import {LetterBlock} from './block.js';
 import {ColorPalette} from './palette.js';
+import {SketchPad} from './sketchpad.js';
 
 export const VIEW_REGISTRY = [
   GlyphRuler,
   LetterBlock,
-  ColorPalette
+  ColorPalette,
+  SketchPad
 ];
---
+++ src/views/sketchpad.js
@@ -0,0 +1,117 @@
+import {View} from './view.js';
+
+export class SketchPad extends View {
+  constructor(...args) {
+    super(...args);
+    this._canvas = this._doc.getElementById('sketchpad');
+    if (!this._canvas.getContext) {
+      const msg = 'No console support detected!';
+      console.error(msg);
+      throw new Error(msg);
+    }
+  }
+
+  draw() {
+    const rect = this._canvas.getBoundingClientRect(),
+          dpr = this._doc.defaultView.devicePixelRatio || 1;
+    console.log('dpr: %d, rect: %o', dpr, rect.width, rect.height);
+    console.log('canvas %d w %d h', this._canvas.width, this._canvas.height);
+
+    this._canvas.width = rect.width * dpr;
+    this._canvas.height = rect.height * dpr;
+
+    this._context = this._canvas.getContext('2d', {alpha: false});
+    this._context.scale(dpr, dpr);
+
+    this._context.clearRect(0, 0, rect.width, rect.height);
+    
+    const bodyStyle = this._doc.defaultView.getComputedStyle(this._doc.getElementsByTagName('body')[0]),
+          fontStyle = `${bodyStyle.getPropertyValue('font-weight')} ${bodyStyle.getPropertyValue('font-size')} ${bodyStyle.getPropertyValue('font-family')}`;
+    this._context.font = fontStyle;
+    this._context.fillStyle = 'red';
+    this._context.fillText('Hello World!', 25, 25);
+    
+    // TODO: figure out how to get this from current brush
+    const glyphDims = {height: 18, width: 9.609375};
+    console.log('Brush dims: %o', glyphDims);
+
+    const letters = ['A', 'a', 'W', '1', 'y', '@'],
+          baseLines = ['alphabetic', 'bottom', 'hanging', 'ideographic', 'middle', 'top'],
+          drawRect = {x: 60, y: 60, w: glyphDims.width, h: glyphDims.height};
+    console.log('draw rect: %o', drawRect);
+    this._context.strokeStyle = '#5a5a5a';
+    this._context.fillStyle = 'blue';
+    const blLength = baseLines.length;
+
+    // test baselines (top places the best)
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = drawRect.x + (i * drawRect.w);
+      this._context.strokeRect(xOffset, drawRect.y, drawRect.w, drawRect.h);
+      this._context.textBaseline = baseLines[i];
+      this._context.fillText('y', xOffset, drawRect.y);
+    }
+    
+    // test varied glyphs
+    drawRect.y += drawRect.h;
+    console.log('draw rect: %o', drawRect);
+    this._context.textBaseline = 'top';
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = drawRect.x + (i * drawRect.w);
+      this._context.strokeRect(xOffset, drawRect.y, drawRect.w, drawRect.h);
+      this._context.fillText(letters[i], xOffset, drawRect.y);
+    }
+
+    // compare to rendered as single string
+    drawRect.y += drawRect.h;
+    console.log('draw rect: %o', drawRect);
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = drawRect.x + (i * drawRect.w);
+      this._context.strokeRect(xOffset, drawRect.y, drawRect.w, drawRect.h);
+    }
+    this._context.fillText(letters.join(''), drawRect.x, drawRect.y);
+
+    // test round-down width
+    const rdRect = Object.assign({}, drawRect);
+    rdRect.y += rdRect.h;
+    rdRect.w = Math.floor(rdRect.w);
+    console.log('draw rect: %o', rdRect);
+    this._context.textBaseline = 'top';
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = rdRect.x + (i * rdRect.w);
+      this._context.strokeRect(xOffset, rdRect.y, rdRect.w, rdRect.h);
+      this._context.fillText(letters[i], xOffset, rdRect.y);
+    }
+
+    // test round-up width
+    const ruRect = Object.assign({}, drawRect);
+    ruRect.y += 2 * ruRect.h;
+    ruRect.w = Math.ceil(ruRect.w);
+    console.log('draw rect: %o', ruRect);
+    this._context.textBaseline = 'top';
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = ruRect.x + (i * ruRect.w);
+      this._context.strokeRect(xOffset, ruRect.y, ruRect.w, ruRect.h);
+      this._context.fillText(letters[i], xOffset, ruRect.y);
+    }
+
+    // test varied glyphs
+    drawRect.y += 3*drawRect.h;
+    console.log('draw rect: %o', drawRect);
+    const weirdLetters = ['►', '↑', '←', '▼', '╩', '╗']
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = drawRect.x + (i * drawRect.w);
+      this._context.strokeRect(xOffset, drawRect.y, drawRect.w, drawRect.h);
+      this._context.fillText(weirdLetters[i], xOffset, drawRect.y);
+    }
+
+    // test varied glyphs
+    drawRect.y += drawRect.h;
+    console.log('draw rect: %o', drawRect);
+    const connectors = ['╚', '╤', '╧', '╦', '═', '╬']
+    for (let i = 0; i < blLength; ++i) {
+      const xOffset = drawRect.x + (i * drawRect.w);
+      this._context.strokeRect(xOffset, drawRect.y, drawRect.w, drawRect.h);
+      this._context.fillText(connectors[i], xOffset, drawRect.y);
+    }
+  }
+}
