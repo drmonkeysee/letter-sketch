8d635f54fe79394af7c3ad7b11d52b009ad0bfa8
Brandon Stansbury <brandonrstansbury@gmail.com>
2019-06-08 20:15:27 -0700

    use temp folder for built assets

--- publish.sh
+++ publish.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
+build_dir=build_tmp
 pub_dir=assets
 
-rm -rf $pub_dir
-mkdir $pub_dir
-npm run build | tee /dev/stderr | awk 'NR > 6 {print $1}' | xargs -I {} cp -v {} $pub_dir
+rm -rf $build_dir
+mkdir $build_dir
+npm run build | tee /dev/stderr | awk 'NR > 6 {print $1}' | xargs -I {} cp -v {} $build_dir
 
 git checkout gh-pages
+rm -rf $pub_dir && mv -v $build_dir $pub_dir
 replace="s/(href|src)=\"\//\1=\"\/$pub_dir\//g"
 sed -E $replace $pub_dir/index.html > index.html
 rm $pub_dir/index.html
