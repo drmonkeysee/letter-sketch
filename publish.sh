fadab001ecc3cffd4bc459303424c9ee7fb82170
Brandon Stansbury <brandonrstansbury@gmail.com>
2019-06-08 20:08:13 -0700

    use explicit git add list

--- publish.sh
+++ publish.sh
@@ -7,11 +7,13 @@ pub_dir=assets
 rm -rf $pub_dir
 mkdir $pub_dir
 npm run build | tee /dev/stderr | awk 'NR > 6 {print $1}' | xargs -I {} mv -v {} $pub_dir
+
 git checkout gh-pages
 replace="s/(href|src)=\"\//\1=\"\/$pub_dir\//g"
 sed -E $replace $pub_dir/index.html > index.html
 rm $pub_dir/index.html
-#git add $pub_dir
-#git commit -am 'publish site'
+
+git add $pub_dir index.html
+#git commit -m 'publish site'
 #git push
 #git checkout master
