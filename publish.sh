d1b4f9be20e8af698108c0e962880ce0b1b22d4a
Brandon Stansbury <brandonrstansbury@gmail.com>
2019-06-08 19:42:39 -0700

    bump package version, fix clean and checkout order

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 rm -rf build_tmp
 mkdir build_tmp
 npm run build | tee /dev/stderr | awk 'NR > 6 {print $1}' | xargs -I {} mv -v {} build_tmp
-git checkout gh-pages
 npm run clean
+git checkout gh-pages
 mv -v build_tmp dist
 sed -E 's/(href|src)="\//\1="\/dist\//g' dist/index.html > index.html
 rm dist/index.html
