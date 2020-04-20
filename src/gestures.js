f8e8cb74bf317165fa03ed31f1ac449bb2c0fc1e
Brandon Stansbury <brandonrstansbury@gmail.com>
2020-04-19 18:01:50 -0700

    factor cursor into its own class

--- src/gestures.js
+++ src/gestures.js
@@ -1,5 +1,94 @@
 import {CP_LOOKUP} from './codepage.js';
 
+const CURSOR_INTERVAL_MS = 600;
+
+class Cursor {
+  constructor(onState, offState, sketchpad, terminal, position) {
+    this.onState = onState;
+    this.offState = offState;
+    this.sketchpad = sketchpad;
+    this.terminal = terminal;
+    this._dimensions = terminal.dimensions;
+    this._initialPosition = this._position = position;
+    this._on = false;
+  }
+
+  get position() { return this._position; }
+
+  get isValidPosition() {
+    return this._position.y >= 0 && this._position.y < this._dimensions.height;
+  }
+
+  start() {
+    this._on = true;
+    this._toggle();
+    this._timer = setInterval(this._toggle.bind(this), CURSOR_INTERVAL_MS);
+  }
+
+  advance() {
+    clearInterval(this._timer);
+    let {x: newX, y: newY} = this._position;
+    ++newX;
+    if (newX >= this._dimensions.width) {
+      newX = 0;
+      ++newY;
+    }
+    // NOTE: always set `position` even when out of bounds to allow
+    // the cursor to "leave" the sketchpad area, otherwise
+    // the user can't type up to the end of the grid.
+    this._position = {x: newX, y: newY};
+    if (newY < this._dimensions.height) {
+      this.start();
+    } else {
+      this.stop();
+    }
+    console.log('NEW CURSOR POS: %o', this._position);
+  }
+
+  reverse() {
+    clearInterval(this._timer);
+    let {x: newX, y: newY} = this._position;
+    --newX;
+    if (newX < 0) {
+      newX = this._dimensions.width - 1;
+      --newY;
+    }
+    if (newX >= this._initialPosition.x || newY > this._initialPosition.y) {
+      this._restoreCell();
+      this._position = {x: newX, y: newY};
+    }
+    this.start();
+    console.log('REV CURSOR POS: %o', this._position);
+  }
+
+  stop() {
+    this._on = false;
+    this._toggle();
+    clearInterval(this._timer);
+  }
+
+  clear() {
+    this.stop();
+    this._restoreCell();
+  }
+
+  _toggle() {
+    this.sketchpad.updateAt(
+      this._position.x,
+      this._position.y,
+      this._on ? this.onState : this.offState
+    );
+    this._on = !this._on;
+  }
+
+  _restoreCell(point) {
+    const cell = this.terminal.getCell(this._position.x, this._position.y);
+    if (cell) {
+      this.sketchpad.updateAt(this._position.x, this._position.y, cell);
+    }
+  }
+}
+
 class Gesture {
   constructor(figureStyle, sketchpad, terminal) {
     this.sketchpad = sketchpad;
@@ -68,17 +157,18 @@ export class MouseGesture extends Gesture {
 }
 
 export class CursorGesture extends Gesture {
-  constructor(...args) {
-    super(...args);
-    this._dimensions = this.terminal.dimensions;
-  }
-
   onMousedown(event) {
     if (this._started) return this.cleanup();
     this._started = true;
-    this._start = this._end = getPoint(event.target);
-    this._activeFigure = this._updateFigure(this._start, this._end);
-    this._setCursor();
+    this._activeFigure = this._updateFigure();
+    this._cursor = new Cursor(
+      this._activeFigure.cursorOn,
+      this._activeFigure.cursorOff,
+      this.sketchpad,
+      this.terminal,
+      getPoint(event.target)
+    );
+    this._cursor.start();
     return null;
   }
 
@@ -102,85 +192,22 @@ export class CursorGesture extends Gesture {
 
   cleanup() {
     if (!this._started) return null;
-    this._clearCursor();
-    this._restoreCell();
+    this._cursor.clear();
     return this._activeFigure;
   }
 
   _advanceCharacter(key) {
     // NOTE: ignore keystroke if not a valid character
     // or cursor is off the edge of the grid.
-    if (this._end.y >= this._dimensions.height || !CP_LOOKUP.has(key)) return;
+    if (!this._cursor.isValidPosition || !CP_LOOKUP.has(key)) return;
 
-    this._activeFigure.advance(this._end, key);
-    clearInterval(this._timer);
+    this._activeFigure.advance(this._cursor.position, key);
     this._drawFigure();
-    this._advanceCursor();
+    this._cursor.advance();
   }
 
   _reverseCharacter() {
     this._activeFigure.reverse();
-    clearInterval(this._timer);
-    this._reverseCursor();
-  }
-
-  _advanceCursor() {
-    let {x: newX, y: newY} = this._end;
-    ++newX;
-    if (newX >= this._dimensions.width) {
-      newX = 0;
-      ++newY;
-    }
-    // NOTE: always set `end` even when out of bounds to allow
-    // the cursor to "leave" the sketchpad area, otherwise
-    // the user can't type up to the end of the grid.
-    this._end = {x: newX, y: newY};
-    if (newY < this._dimensions.height) {
-      this._setCursor();
-    } else {
-      this._clearCursor();
-    }
-    console.log('NEW END: %o', this._end);
-  }
-
-  _reverseCursor() {
-    let {x: newX, y: newY} = this._end;
-    --newX;
-    if (newX < 0) {
-      newX = this._dimensions.width - 1;
-      --newY;
-    }
-    if (newX >= this._start.x && newY >= this._start.y) {
-      this._restoreCell();
-      this._end = {x: newX, y: newY};
-    }
-    this._setCursor();
-  }
-
-  _setCursor() {
-    this._cursorOn = true;
-    this._toggleCursor();
-    this._timer = setInterval(this._toggleCursor.bind(this), 600);
-  }
-
-  _clearCursor() {
-    this._cursorOn = false;
-    this._toggleCursor();
-    clearInterval(this._timer);
-  }
-
-  _toggleCursor() {
-    const cursorCell = this._cursorOn
-                          ? this._activeFigure.cursorOn
-                          : this._activeFigure.cursorOff;
-    this.sketchpad.updateAt(this._end.x, this._end.y, cursorCell);
-    this._cursorOn = !this._cursorOn;
-  }
-
-  _restoreCell(point) {
-    const cell = this.terminal.getCell(this._end.x, this._end.y);
-    if (cell) {
-      this.sketchpad.updateAt(this._end.x, this._end.y, cell);
-    }
+    this._cursor.reverse();
   }
 }
