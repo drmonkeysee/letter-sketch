a7e19a9ae4d8be2e02ada48e8f6199b95b590427
Brandon Stansbury <brandonrstansbury@gmail.com>
2020-11-20 22:46:50 -0800

    add codepage tests

---
+++ test/codepage.js
@@ -0,0 +1,31 @@
+import {expect} from 'chai';
+
+import codepage from '../src/codepage.js';
+
+describe('#glyph()', function () {
+  it('returns expected value for id', function () {
+    const result = codepage.glyph(0x41);
+
+    expect(result).to.equal('A');
+  });
+
+  it('returns undefined if invalid id', function () {
+    const result = codepage.glyph(1000);
+
+    expect(result).to.be.undefined;
+  });
+});
+
+describe('#id()', function () {
+  it('returns expected id for glyph', function() {
+    const result = codepage.id('#');
+
+    expect(result).to.equal(0x23);
+  });
+
+  it('returns invalid id for unknown glyph', function () {
+    const result = codepage.id('元');
+
+    expect(result).to.equal(-1);
+  });
+});
