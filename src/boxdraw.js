4486ccb00ea42747df97cfd59c61b148886f41a5
Brandon Stansbury <brandonrstansbury@gmail.com>
2021-09-05 21:16:18 -0700

    wire up double line set

--- src/boxdraw.js
+++ src/boxdraw.js
@@ -1,34 +1,65 @@
 const SINGLE_LINES = {
-  179: 0b0101,
-  180: 0b1101,
-  191: 0b1100,
-  192: 0b0011,
-  193: 0b1011,
-  194: 0b1110,
-  195: 0b0111,
-  196: 0b1010,
-  197: 0b1111,
-  217: 0b1001,
-  218: 0b0110,
-};
-const SINGLE_BEST_FIT = [
-  196,  // 0000
-  179,  // 0001
-  196,  // 0010
-  192,  // 0011
-  179,  // 0100
-  179,  // 0101
-  218,  // 0110
-  195,  // 0111
-  196,  // 1000
-  217,  // 1001
-  196,  // 1010
-  193,  // 1011
-  191,  // 1100
-  180,  // 1101
-  194,  // 1110
-  197,  // 1111
-];
+        179: 0b0101,
+        180: 0b1101,
+        191: 0b1100,
+        192: 0b0011,
+        193: 0b1011,
+        194: 0b1110,
+        195: 0b0111,
+        196: 0b1010,
+        197: 0b1111,
+        217: 0b1001,
+        218: 0b0110,
+      },
+      SINGLE_BEST_FIT = [
+        196,  // 0000
+        179,  // 0001
+        196,  // 0010
+        192,  // 0011
+        179,  // 0100
+        179,  // 0101
+        218,  // 0110
+        195,  // 0111
+        196,  // 1000
+        217,  // 1001
+        196,  // 1010
+        193,  // 1011
+        191,  // 1100
+        180,  // 1101
+        194,  // 1110
+        197,  // 1111
+      ],
+      DOUBLE_LINES = {
+        185: 0b1101,
+        186: 0b0101,
+        187: 0b1100,
+        188: 0b1001,
+        200: 0b0011,
+        201: 0b0110,
+        202: 0b1011,
+        203: 0b1110,
+        204: 0b0111,
+        205: 0b1010,
+        206: 0b1111,
+      },
+      DOUBLE_BEST_FIT = [
+        205,  // 0000
+        186,  // 0001
+        205,  // 0010
+        200,  // 0011
+        186,  // 0100
+        186,  // 0101
+        201,  // 0110
+        204,  // 0111
+        205,  // 1000
+        188,  // 1001
+        205,  // 1010
+        202,  // 1011
+        187,  // 1100
+        185,  // 1101
+        203,  // 1110
+        206,  // 1111
+      ];
 
 class LineSet {
   constructor(lines, bestFit) {
@@ -45,7 +76,8 @@ class LineSet {
   }
 }
 
-const singleLineSet = new LineSet(SINGLE_LINES, SINGLE_BEST_FIT);
+const singleLineSet = new LineSet(SINGLE_LINES, SINGLE_BEST_FIT),
+      doubleLineSet = new LineSet(DOUBLE_LINES, DOUBLE_BEST_FIT);
 
 export const DIRECTIONS = {
   NONE: 0b0000,
@@ -57,6 +89,7 @@ export const DIRECTIONS = {
 
 export function getLineSet(glyphId) {
   if (SINGLE_LINES[glyphId]) return singleLineSet;
+  if (DOUBLE_LINES[glyphId]) return doubleLineSet;
   return null;
 }
 
